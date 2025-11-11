8bee56495297d793a4b4f8580f8c7090a4fa5b11
Siyona Snotra <141633032+siyonasnotra@users.noreply.github.com>
2025-11-11 22:58:41 +0530

    Update resume URL to new Google Drive link

--- src/data/resume.ts
+++ src/data/resume.ts
@@ -2,4 +2,5 @@
 // Example: "/resume/siyona-snotra-resume.pdf" (if file is in public/resume/)
 // or a direct URL like "https://example.com/resume.pdf"
 
-export const RESUME_URL: string | null = "https://drive.google.com/file/d/1k1TuKYqqZb89y8xukHFv_bpQkenoL1_c/view?usp=drive_link";
+
+export const RESUME_URL: string | null = "https://drive.google.com/file/d/1MZbblUaCFH25YtrvnH_JwK7mVESsD2eY/view?usp=drivesdk";
